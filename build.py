9eb62a84104f8a4b9865430319bf912a2ed1f05f
Martin Valgur <martin.valgur@gmail.com>
2023-06-23 14:53:35 +0300

    Tidy build.py

--- build.py
+++ build.py
@@ -1,42 +1,68 @@
+#!/usr/bin/env python
+
+import os
+import sys
+from contextlib import contextmanager
+
 from conans.client import conan_api
 from cpt.packager import ConanMultiPackager
-import os
+
+
+@contextmanager
+def chdir(new_dir):
+    cwd = os.getcwd()
+    try:
+        os.chdir(new_dir)
+        yield
+    finally:
+        os.chdir(cwd)
+
 
 def inspect_value_from_recipe(attribute, recipe_path):
-    cwd = os.getcwd()
-    result = None
-    try:
-        dir_name = os.path.dirname(recipe_path)
-        os.chdir("./" if dir_name == "" else dir_name)
+    dir_name = os.path.dirname(recipe_path) or "./"
+    with chdir(dir_name):
         conan_instance, _, _ = conan_api.Conan.factory()
         inspect_result = conan_instance.inspect(path=os.path.basename(recipe_path), attributes=[attribute])
         result = inspect_result.get(attribute)
-    except:
-        pass
-    os.chdir(cwd)
-    return result
+        return result
+
 
 def get_repo_branch_from_githubaction():
-    def _clean_branch(branch):
-        return branch[11:] if branch.startswith("refs/heads/") else branch
-    repobranch = _clean_branch(os.getenv("GITHUB_REF", ""))
-    if os.getenv("GITHUB_EVENT_NAME", "") == "pull_request":
-        repobranch = os.getenv("GITHUB_BASE_REF", "")
-    return repobranch
+    branch = os.getenv("GITHUB_REF", "")
+    if os.getenv("GITHUB_EVENT_NAME") == "pull_request":
+        return os.getenv("GITHUB_BASE_REF", "")
+    elif branch.startswith("refs/heads/"):
+        return branch[11:]
+    else:
+        return branch
+
 
 def has_shared_option(recipe_path):
     options = inspect_value_from_recipe(attribute="options", recipe_path=recipe_path)
     return options and "shared" in options
 
+
 if __name__ == "__main__":
     recipe_path = os.path.abspath("conanfile.py")
     recipe_name = inspect_value_from_recipe(attribute="name", recipe_path=recipe_path)
-    channel, recipe_version = get_repo_branch_from_githubaction().split("/")
-    reference = "{}/{}".format(recipe_name, recipe_version)
-    shared_option_name = "{}:shared".format(recipe_name) if has_shared_option(recipe_path) else None
+    if len(sys.argv) > 1:
+        recipe_version = sys.argv[1]
+    else:
+        branch = get_repo_branch_from_githubaction()
+        recipe_version = branch.rsplit("/")[-1]
+    reference = f"{recipe_name}/{recipe_version}"
+    shared_option_name = f"{recipe_name}:shared" if has_shared_option(recipe_path) else None
 
-    builder = ConanMultiPackager(build_policy="missing", skip_check_credentials=True,
-                                 docker_run_options="-u 0:0")
-    builder.add_common_builds(shared_option_name=shared_option_name, pure_c=False, dll_with_static_runtime=True,
-                              reference=reference, build_all_options_values=None)
+    builder = ConanMultiPackager(
+        build_policy="missing",
+        skip_check_credentials=True,
+        docker_run_options="-u 0:0",
+    )
+    builder.add_common_builds(
+        shared_option_name=shared_option_name,
+        pure_c=False,
+        dll_with_static_runtime=True,
+        reference=reference,
+        build_all_options_values=None,
+    )
     builder.run()
